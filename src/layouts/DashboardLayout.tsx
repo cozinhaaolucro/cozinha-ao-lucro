d3680f897ab58813a4c6f1443b98a73f82f95317
cozinhaaolucro <cozinhaaolucro@gmail.com>
2025-12-14 04:21:03 -0300

    Update DashboardLayout.tsx

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -15,7 +15,8 @@ import {
     Camera,
     Loader2,
     Bell,
-    X
+    X,
+    ShoppingCart
 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
